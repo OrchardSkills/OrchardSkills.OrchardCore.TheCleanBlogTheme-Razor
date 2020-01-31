d38ca1d8c0a9f2b1573fa20beee19b2213955dbe
Orchard Skills <Sales@OrchardSkills.com>
2020-01-31 07:12:38 -0700

    Refactored name TheCleanBlogTheme

---
+++ OrchardSkills.OrchardCore.TheCLeanBlogTheme/Manifest.cs
@@ -0,0 +1,10 @@
+using OrchardCore.Modules.Manifest;
+
+[assembly: Module(
+    Name = "TheCleanBlogTheme",
+    Author = "OrchardSkills.com",
+    Website = "http://OrchardSkills.com",
+    Version = "2.0.0",
+    Description = "The Clean Blog Theme."
+)]
+
---
+++ OrchardSkills.OrchardCore.TheCLeanBlogTheme/Startup.cs
@@ -0,0 +1,16 @@
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
+using OrchardCore.Modules;
+using OrchardCore.ResourceManagement;
+
+namespace OrchardCore.Themes.TheCleanBlogTheme
+
+{
+    public class Startup : StartupBase
+    {
+        public override void ConfigureServices(IServiceCollection serviceCollection)
+        {
+            serviceCollection.AddScoped<IResourceManifestProvider, ResourceManifest>();
+        }
+    }
+}
